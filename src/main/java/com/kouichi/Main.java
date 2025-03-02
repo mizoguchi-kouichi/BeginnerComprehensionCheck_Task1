3fd4b54bfca5d07b38bc5257736b1f081f606574
”溝口光一” <guangyigoukou7@gmail.com>
2025-03-02 15:08:28 +0900

    計算機システムの作成

---
+++ src/main/java/com/kouichi/Main.java
@@ -0,0 +1,31 @@
+package com.kouichi;
+
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    int total = 0;
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("1番目の数字を入力してください:");
+
+    int firstNumber = scanner.nextInt();
+    System.out.print("演算子を入力してください(+, -, *, /):");
+    String operator = scanner.next();
+    System.out.print("2番目の数字を入力してください");
+    int secondNumber = scanner.nextInt();
+    System.out.print("\r\n");
+
+    total = switch (operator) {
+      case "+" -> firstNumber + secondNumber;
+      case "-" -> firstNumber - secondNumber;
+      case "*" -> firstNumber * secondNumber;
+      case "/" -> firstNumber / secondNumber;
+      default -> total;
+    };
+    System.out.println("期待される出力例:");
+    System.out.printf("   計算結果:" + firstNumber + operator + secondNumber + "=" + total);
+
+  }
+}
